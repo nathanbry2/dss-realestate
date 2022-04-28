61e51180abec4ffc64f7930ec2b82983547a7a43
admin <admin>
2022-04-28 17:37:14 +0200

    edit files

--- lib/python/geoloc_functions/__init__.py
+++ lib/python/geoloc_functions/__init__.py
@@ -1,5 +1,5 @@
 import numpy as np
-from math import sin, cos, sqrt, atan2, radians
+from math import sin, cos, sqrt, atan2, radians, pi
 
 ''' R = approximate radius of earth in km,
     centre cordinates correspond to Place Dauphine coordinates (which is considered as Paris surface centre)
@@ -18,3 +18,22 @@ def distance_to_centre(lat, long, R = 6373.0, lat_centre = 48.8565, long_centre 
     distance = R * c
    
     return distance
+
+'''
+
+
+    '''
+
+def direction_lookup(destination_long, destination_lat, origin_lat = 48.853, origin_long = 2.35):
+    deltaX = destination_long - origin_long
+    deltaY = destination_lat - origin_lat
+    degrees_temp = atan2(deltaX, deltaY)/math.pi*180
+    if degrees_temp < 0:
+        degrees_final = 360 + degrees_temp
+    else:
+        degrees_final = degrees_temp
+
+    compass_brackets = ["N", "NE", "E", "SE", "S", "SW", "W", "NW", "N"]
+    compass_lookup = round(degrees_final / 45)
+
+    return compass_brackets[compass_lookup]#, degrees_final
