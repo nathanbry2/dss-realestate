4a595fa4b391a437682b9928dfd4da7ab164243d
admin <admin>
2022-04-29 01:42:30 +0200

    Recipe REALESTATE.compute_transactions_enriched saved through notebook notebook editor for compute_transactions_enriched

--- recipes/compute_transactions_enriched.py
+++ recipes/compute_transactions_enriched.py
@@ -4,6 +4,8 @@ import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 from geoloc_functions import distance_to_centre, direction_from_centre
+from openlocationcode import openlocationcode as olc
+from pluscode_functions import convert_dic, convert_pluscode
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
@@ -20,6 +22,25 @@ v75_2014_2021_transactions_partial_pluscode_df['direction_from_centre'] = np.vec
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 v75_2014_2021_transactions_partial_pluscode_df
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+data_encoded = v75_2014_2021_transactions_partial_pluscode_df[v75_2014_2021_transactions_partial_pluscode_df['pluscode_16_first'].isnull() == False]
+data_to_encode = v75_2014_2021_transactions_partial_pluscode_df[v75_2014_2021_transactions_partial_pluscode_df['pluscode_16_first'].isnull() == True]
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+data_to_encode['pluscode_16_first'] = np.vectorize(olc.encode)(data_to_encode['latitude'],data_to_encode['longitude'],17)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+full_data = pd.concat([data_encoded,data_to_encode])
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+full_data['pluscode_10'] = [code[:11] for code in full_data['pluscode_16_first'].to_list()]
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+full_data['v_coordinate'],full_data['h_coordinate'],full_data['reduced_v_coordinate'],full_data['reduced_h_coordinate'],full_data['reduced_coordinates_couple'] = convert_pluscode(full_data['pluscode_10'])
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+full_data
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs
