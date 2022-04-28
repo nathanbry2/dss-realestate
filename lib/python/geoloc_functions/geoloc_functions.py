26b61d101bd63d84f38e596399b9ec987e009877
admin <admin>
2022-04-28 17:26:43 +0200

    edit files

---
+++ lib/python/geoloc_functions/geoloc_functions.py
@@ -0,0 +1,14 @@
+# R = approximate radius of earth in km,
+# centre cordinates correspond to Place Dauphine coordinates (which is considered as Paris surface centre)
+
+def distance_to_centre(lat, long, R = 6373.0, lat_centre = 48.8565, long_centre = 2.3424):
+   
+    dlon = np.radians(long) - np.radians(long_centre)
+    dlat = np.radians(lat) - np.radians(lat_centre)
+
+    a = np.sin(dlat / 2)**2 + np.cos(np.radians(lat)) * np.cos(np.radians(lat_centre)) * np.sin(dlon / 2)**2
+    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
+
+    distance = R * c
+   
+    return distance
