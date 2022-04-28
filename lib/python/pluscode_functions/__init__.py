376fa793bd5b266014c6052e55b2eb570c87a9ab
admin <admin>
2022-04-29 01:33:39 +0200

    edit files

--- lib/python/pluscode_functions/__init__.py
+++ lib/python/pluscode_functions/__init__.py
@@ -1,3 +1,5 @@
+from math import floor
+
 convert_dic = {
     '2':0,
     '3':1,
@@ -20,3 +22,34 @@ convert_dic = {
     'W':18,
     'X':19,
 }
+
+
+def convert_pluscode(pluscode_col):
+    
+    v_list = []
+    h_list = []
+    reduced_v_list = []
+    reduced_h_list = []
+    reduced_coordinates_list = []
+
+    pluscode_list = pluscode_col.to_list()
+    
+    for code in pluscode_list:
+        v = 0
+        h = 0
+
+        v += (convert_dic[code[4]]-16)*20
+        h += (convert_dic[code[5]]-5)*20
+        v += (convert_dic[code[6]])
+        h += (convert_dic[code[7]])
+        #v += (convert_dic[code[9]])
+        #h += (convert_dic[code[10]])
+
+        v_list.append(v)
+        h_list.append(h)
+
+        reduced_v_list.append(floor(v/2))
+        reduced_h_list.append(floor(h/2))
+        reduced_coordinates_list.append(str(floor(v/2))+','+str(floor(h/2)))
+        
+    return v_list, h_list, reduced_v_list, reduced_h_list, reduced_coordinates_list
