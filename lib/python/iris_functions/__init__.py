0de026a121ead2b3a00ddac5324d92740ffc6ee7
admin <admin>
2022-04-29 20:07:34 +0200

    edit files

--- lib/python/iris_functions/__init__.py
+++ lib/python/iris_functions/__init__.py
@@ -31,3 +31,44 @@ def create_iris_polygon_dict(iris_code_column,polygon_column):
     iris_coords_codes = dict(zip(iris_code_column.to_list(),polygon_list))
     
     return iris_coords_codes 
+
+'''
+    This functions creates GeoPoints from latitude and longitude coordinates, and finds the iris corresponding to these points.
+    It takes as inputs the latitude and longitude columns of a dataframe, the dictionary and returns a dictionary.
+'''
+
+
+def find_iris(latitude_column, longitude_column, iris_coords_codes_dict):
+    lat_list = latitude_column.to_list()
+    long_list = longitude_column.to_list()
+
+    points_list = []
+
+    for i in range(len(lat_list)):
+        coords_point = Point(long_list[i],lat_list[i])
+
+        points_list.append(coords_point)
+        
+        
+    points_iris_code_list = []
+
+    for point in points_list:
+
+        is_within_polygon = False
+
+        for iris,coords in iris_coords_codes_dict.items():
+
+            test = point.within(coords)
+
+            if test == True:
+
+                points_iris_code_list.append(iris)
+                is_within_polygon = True
+
+                break
+
+        if is_within_polygon==False:
+
+            points_iris_code_list.append('None')
+            
+    return points_iris_code_list
