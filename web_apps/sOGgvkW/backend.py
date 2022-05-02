27af2c6dee367865f4915be87e34b2eb0309fb69
admin <admin>
2022-05-02 16:41:24 +0200

    Saved web app EstimationInterface (REALESTATE.sOGgvkW)

--- web_apps/sOGgvkW/backend.py
+++ web_apps/sOGgvkW/backend.py
@@ -2,6 +2,7 @@ import plotly.express as px
 import dataiku
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 import pandas as pd
 from dash.dependencies import Input, Output, State
 
