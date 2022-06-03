15f0516d031931e5d1f255518906166243d26f42
admin <admin>
2022-06-03 23:12:57 +0200

    Recipe REALESTATE.compute_inputs_located saved through notebook notebook editor for compute_inputs_located

--- recipes/compute_inputs_located.py
+++ recipes/compute_inputs_located.py
@@ -3,12 +3,51 @@
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
-from geopy.geocoders import Nominatim
+from geoloc_functions import find_location,distance_to_centre, direction_from_centre
+from openlocationcode import openlocationcode as olc
+from pluscode_functions import convert_dic, convert_pluscode
+from iris_functions import create_iris_polygon_dict, find_iris
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
 new_inputs = dataiku.Dataset("new_inputs")
 new_inputs_df = new_inputs.get_dataframe()
+iris_75 = dataiku.Dataset("iris_75")
+iris_75_df = iris_75.get_dataframe()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+new_inputs_df
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+new_inputs_df['latitude'] = find_location(new_inputs_df['adresse'])[0]
+new_inputs_df['longitude'] = find_location(new_inputs_df['adresse'])[1]
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+new_inputs_df
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+new_inputs_df['distance_to_centre']= distance_to_centre(new_inputs_df['latitude'],new_inputs_df['longitude'])
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+new_inputs_df['direction_from_centre'] = np.vectorize(direction_from_centre)(new_inputs_df['longitude'],new_inputs_df['latitude'])
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+new_inputs_df
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+new_inputs_df['pluscode_16_first'] = np.vectorize(olc.encode)(new_inputs_df['latitude'],new_inputs_df['longitude'],17)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+new_inputs_df['pluscode_10'] = [code[:11] for code in new_inputs_df['pluscode_16_first'].to_list()]
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+new_inputs_df['v_coordinate'],new_inputs_df['h_coordinate'],new_inputs_df['reduced_v_coordinate'],new_inputs_df['reduced_h_coordinate'],new_inputs_df['reduced_coordinates_couple'] = convert_pluscode(new_inputs_df['pluscode_10'])
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+iris_coords_codes = create_iris_polygon_dict(iris_75_df['CODE_IRIS'],iris_75_df['Geo Shape'])
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+new_inputs_df['iris_code'] = find_iris(new_inputs_df['latitude'],new_inputs_df['longitude'],iris_coords_codes)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs from inputs
