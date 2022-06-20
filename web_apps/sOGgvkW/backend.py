bec62392ec25e60b64cf92c0d9632663a6a0792e
admin <admin>
2022-06-20 12:58:56 +0200

    Saved web app EstimationInterface (REALESTATE.sOGgvkW)

--- web_apps/sOGgvkW/backend.py
+++ web_apps/sOGgvkW/backend.py
@@ -97,7 +97,7 @@ app.layout = html.Div(
                         value='',
                         placeholder="Address",
                         style = {
-                            #'margin-left':input_style['margin-left']
+                            'margin-left':input_style['margin-left']
                         }
                     )
                 ),
@@ -108,7 +108,7 @@ app.layout = html.Div(
                         value='',
                         placeholder="Postal Code",
                         style = {
-                            #'margin-left':input_style['margin-left']
+                            'margin-left':input_style['margin-left']
                         }
                     )
                 ),
@@ -119,7 +119,7 @@ app.layout = html.Div(
                         value='',
                         placeholder="Surface",
                         style = {
-                            #'margin-left':input_style['margin-left']
+                            'margin-left':input_style['margin-left']
                         }
                     )
                 ),
