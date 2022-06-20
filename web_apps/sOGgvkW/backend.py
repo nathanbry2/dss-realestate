d00cdc966d50cd2aae3db2bb7b8c98763fc17ae0
admin <admin>
2022-06-20 17:40:40 +0200

    Saved web app EstimationInterface (REALESTATE.sOGgvkW)

--- web_apps/sOGgvkW/backend.py
+++ web_apps/sOGgvkW/backend.py
@@ -205,7 +205,7 @@ app.layout = html.Div(
 
 def output_function(n_clicks,input1,input2,input3,input4,input5):
     
-    df = pd.DataFrame([[input1,input2,input3,input4,input5]],columns=['address','postal_code','surface','nb_main_rooms','year'])
+    #df = pd.DataFrame([[input1,input2,input3,input4,input5]],columns=['address','postal_code','surface','nb_main_rooms','year'])
     return input1,input2,input3,input4,input5
 
 #dash_table.DataTable(data=df.to_dict('records'),columns=[{"name": i, "id": i} for i in df.columns])
