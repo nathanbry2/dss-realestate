c7d6b32569731a735b31f302969742634cad51d0
admin <admin>
2022-06-23 23:35:21 +0200

    Saved web app EstimationInterface (REALESTATE.sOGgvkW)

--- web_apps/sOGgvkW/backend.py
+++ web_apps/sOGgvkW/backend.py
@@ -163,7 +163,7 @@ app.layout = html.Div(
                     html.Img(
                         src='data:image/png;base64,{}'.format(estimate_logo_encoded.decode()),         
                         style={
-                            'width':'65%',
+                            'width':'25%',
                             'height':'25%',
                         }
                     ), 
