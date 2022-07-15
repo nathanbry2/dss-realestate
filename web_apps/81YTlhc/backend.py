0b343dfa5f14da4bbca79e3f39f1a6999a4f8a2b
admin <admin>
2022-07-15 14:58:55 +0200

    Saved web app v2 (REALESTATE.81YTlhc)

--- web_apps/81YTlhc/backend.py
+++ web_apps/81YTlhc/backend.py
@@ -107,7 +107,7 @@ app.layout = html.Div(
                             id="input1",
                             type='text',
                             value='',
-                            placeholder="Address",
+                            placeholder="Address, ex: 13 rue saulnier",
                             ),html.Br(),html.Br(),
                         
                         dcc.Input(
