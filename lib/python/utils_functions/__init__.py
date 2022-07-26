c8fe236d92df6423dedc5f484fe30fc1789eee2a
admin <admin>
2022-07-26 11:45:54 +0200

    edit files

--- lib/python/utils_functions/__init__.py
+++ lib/python/utils_functions/__init__.py
@@ -14,3 +14,30 @@ def correct_date(date_column):
         new_dates_list.append(new_date)
 
     return new_dates_list
+
+
+
+def df_to_geojson(df, properties, geo_col):
+    # create a new python dict to contain our geojson data, using geojson format
+    geojson = {'type':'FeatureCollection', 'features':[]}
+
+    # loop through each row in the dataframe and convert each row to geojson format
+    for _, row in df.iterrows():
+        # create a feature template to fill in
+        feature = {'type':'Feature',
+                   'properties':{},
+                   'geometry':{}}
+
+        # fill in the coordinates
+        feature['geometry'] = ast.literal_eval(row[geo_col])
+        
+        feature['id'] = row['NOM_IRIS_first']
+
+        # for each column, get the value and add it as a new feature property
+        for prop in properties:
+            feature['properties'][prop] = row[prop]
+        
+        # add this feature (aka, converted dataframe row) to the list of features inside our dict
+        geojson['features'].append(feature)
+    
+    return geojson
