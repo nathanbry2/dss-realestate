8f71e71b7ff7b66eb54a440dd221967cd19395a3
admin <admin>
2022-07-27 18:21:53 +0200

    Saved web app v2 (REALESTATE.81YTlhc)

--- web_apps/81YTlhc/backend.py
+++ web_apps/81YTlhc/backend.py
@@ -390,11 +390,13 @@ def output_function(n_clicks,input1,input2,input3,input4,input5):
                           text = [df_filtered.iloc[-1].NOM_IRIS_first],  #a list of strings, one  for each geographical position  (lon, lat)              
                           below='',marker=dict(size=10, color='#221C35',symbol='circle'),               
                           )
-    fig.update_layout(margin={"r":10,"t":50,"l":20,"b":20},
+    fig.update_layout(autosize=False,
+    width=500,
+    height=500,
                       title={
                           'text': "<b>Average m² price (€) in Paris areas - 2021</b>",
                       })
     #fig2.show()
-    
+    #,style={'width': 'auto','height':'300px','border':'none'}
     
     return text1, text2, text3, text4, text5, text6, dcc.Graph(figure=fig), fig2
