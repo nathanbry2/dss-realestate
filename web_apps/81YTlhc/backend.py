5c8552a3b81abe22119710732748c6af41d6219b
admin <admin>
2022-07-27 18:03:00 +0200

    Saved web app v2 (REALESTATE.81YTlhc)

--- web_apps/81YTlhc/backend.py
+++ web_apps/81YTlhc/backend.py
@@ -251,7 +251,7 @@ app.layout = html.Div(
                     }
                 ),html.Br(),html.Br(),
                 html.Div(id='output7',style={'height': '250%'}),html.Br(),html.Br(),
-                html.Div(id='output8',style={'height': 500}),html.Br(),html.Br(),
+                html.Div(id='output8',style={'height': 1500}),html.Br(),html.Br(),
             ]
         )
         
