c69e4490d962089609e69814d8e3c205f463b67e
admin <admin>
2022-07-28 18:04:12 +0200

    Saved web app v3 (REALESTATE.y3ortWT)

--- web_apps/y3ortWT/backend.py
+++ web_apps/y3ortWT/backend.py
@@ -236,7 +236,7 @@ app.layout = html.Div(
                                     }
                                 ),html.Br(),
                                 html.Ul(id='list_output',style={
-                                    "display": "block",
+                                    "display": "inline",
                                     'gap': "15px",
                                     'font-size': "20px",
                                     'justify-content': 'center'}),
