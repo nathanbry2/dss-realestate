c6d46e5905f4c9babf4d0fede368f791f3bda14b
admin <admin>
2022-07-28 09:17:07 +0200

    Saved web app v2 (REALESTATE.81YTlhc)

--- web_apps/81YTlhc/backend.py
+++ web_apps/81YTlhc/backend.py
@@ -252,7 +252,7 @@ app.layout = html.Div(
                 ),html.Br(),html.Br(),
                 html.Div(id='output7',style={'height': '250%'}),html.Br(),html.Br(),
                 #html.Div(id='output8',style={'width': 'auto','height':'300px','border':'none'}),html.Br(),html.Br(),
-                dcc.Graph(id='output8',style={'width': 'auto','height':'650px','border':'none'})
+                dcc.Graph(id='output8',figure = blank_figure(),style={'width': 'auto','height':'650px','border':'none'})
             ]
         )
         
