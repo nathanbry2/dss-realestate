20ad77d510a822ed878d4dc3254b4895a9a80478
admin <admin>
2022-08-01 22:05:44 +0200

    Saved web app v3 (REALESTATE.y3ortWT)

--- web_apps/y3ortWT/backend.py
+++ web_apps/y3ortWT/backend.py
@@ -94,7 +94,7 @@ app.layout = html.Div(
                         #'backgroundColor': title_style['background'],
                         'color': title_style['text'],
                         'font-weight':'bold',
-                        'margin': 'auto auto auto 100px',
+                        'margin': 'auto auto auto 1%',
                     }
                 )
             ],
