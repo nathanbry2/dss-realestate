defd373cbe955e9dcb7bec5dce2dc9c767f5eb60
admin <admin>
2022-08-03 10:58:18 +0200

    Saved web app v3 (REALESTATE.y3ortWT)

--- web_apps/y3ortWT/backend.py
+++ web_apps/y3ortWT/backend.py
@@ -186,7 +186,7 @@ app.layout = html.Div(
                     
                     style = {
                         'text-align':'center',
-                        "margin-left":"3%",
+                        "margin-left":"5%",
                         #"margin-right":"20%",
                     },
                 
