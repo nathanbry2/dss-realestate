1d8f238d7651da5336d46d2fb2fb97fb05fd2e02
admin <admin>
2022-08-08 19:57:05 +0200

    Saved web app v3 (REALESTATE.y3ortWT)

--- web_apps/y3ortWT/backend.py
+++ web_apps/y3ortWT/backend.py
@@ -563,8 +563,9 @@ def output_function(n_clicks,input1,input2,input3,input4,input5):
     
     return text1, text2, [html.Li(dcc.Markdown(i)) for i in text_list], dcc.Graph(figure=fig), fig2
 
-
+'''
 @app.callback(Output("anchor", "id"), Input("submit", "id"))
 def input_triggers_spinner(value):
     time.sleep(1)
     return value
+'''
