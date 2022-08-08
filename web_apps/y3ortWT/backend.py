f0afece4afb6494eb64d1b9e133fb2b8475e4450
admin <admin>
2022-08-08 15:10:26 +0200

    Saved web app v3 (REALESTATE.y3ortWT)

--- web_apps/y3ortWT/backend.py
+++ web_apps/y3ortWT/backend.py
@@ -118,8 +118,8 @@ app.layout = html.Div(
                 
                 html.Br(),html.Br(),
                 html.Div(
-                    html.H3(
-                        children='Welcome to the Dataiku Real Estate Estimator! <br> Fill in the following information about your property, and click on the button to get an estimation of how much it is worth 💰',
+                    dcc.Markdown(
+                        children='Welcome to the Dataiku Real Estate Estimator! Fill in the following information about your property, and click on the button to get an estimation of how much it is worth 💰',
                         style={
                             #'backgroundColor': desc_style['background'],
                             'color': desc_style['text'],
