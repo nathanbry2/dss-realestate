830b9cb140f2586c7638af0f8bab30a141248a30
admin <admin>
2022-08-08 10:49:18 +0200

    Saved web app v3 (REALESTATE.y3ortWT)

--- web_apps/y3ortWT/backend.py
+++ web_apps/y3ortWT/backend.py
@@ -209,7 +209,7 @@ app.layout = html.Div(
                             ), 
                             id="submit",
                             n_clicks=0,
-                            color='primary',
+                            color='info',
                             #color='#00B2A9',
                             outline=True,
                             style = {
