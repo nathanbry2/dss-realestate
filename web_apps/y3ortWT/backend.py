d0c28b7149894aa135c7684646dd9b571f5fb0bc
admin <admin>
2022-08-08 14:43:38 +0200

    Saved web app v3 (REALESTATE.y3ortWT)

--- web_apps/y3ortWT/backend.py
+++ web_apps/y3ortWT/backend.py
@@ -158,7 +158,7 @@ app.layout = html.Div(
                                 #"margin-right":"40%",
                                 #'background-color': '#00B2A9',
                                 'border-color':'#00B2A9',
-                                'border-width':"2.5px",
+                                'border-width':"2px",
 
                             }
                             ),html.Br(),html.Br(),
