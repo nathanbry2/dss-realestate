b3d23c919d672ec54f3873646754915899206564
admin <admin>
2022-08-09 10:35:46 +0200

    Saved web app v3 (REALESTATE.y3ortWT)

--- web_apps/y3ortWT/backend.py
+++ web_apps/y3ortWT/backend.py
@@ -227,9 +227,14 @@ app.layout = html.Div(
                         dbc.Select(
                             id="select",
                             options=[
-                                {"label": "Option 1", "value": "1"},
-                                {"label": "Option 2", "value": "2"},
-                                {"label": "Disabled option", "value": "3", "disabled": True},
+                                {"label": "2014", "value": 2014},
+                                {"label": "2015", "value": 2015},
+                                {"label": "2016", "value": 2016},
+                                {"label": "2017", "value": 2017},
+                                {"label": "2018", "value": 2018},
+                                {"label": "2019", "value": 2019},
+                                {"label": "2020", "value": 2020},
+                                {"label": "2021", "value": 2021},
                             ],
                         ),
                     ],
