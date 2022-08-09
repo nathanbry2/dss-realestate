f0b82f39062e9a719f8515a9fd5e1db54d769cee
admin <admin>
2022-08-09 16:44:01 +0200

    Saved web app v4 (REALESTATE.RbecpI4)

--- web_apps/RbecpI4/backend.py
+++ web_apps/RbecpI4/backend.py
@@ -355,7 +355,7 @@ app.layout = html.Div(
         html.Br(),html.Br(),html.Br(),
         dcc.Loading(
             id="loading-1",
-            type="graph",
+            type="dot",
             color='#00B2A9',
             children=html.Div(id="loading-output-1")
         ),html.Br(),html.Br(),html.Br(),html.Br(),
