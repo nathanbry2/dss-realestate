056b1c6ff59bb1bb9a2e334d07dae89374fd618b
admin <admin>
2022-08-09 10:34:12 +0200

    Saved web app v3 (REALESTATE.y3ortWT)

--- web_apps/y3ortWT/backend.py
+++ web_apps/y3ortWT/backend.py
@@ -222,7 +222,16 @@ app.layout = html.Div(
                                 'border-width':"2px",
 
                             }
-                            )
+                            ),
+                        
+                        dbc.Select(
+                            id="select",
+                            options=[
+                                {"label": "Option 1", "value": "1"},
+                                {"label": "Option 2", "value": "2"},
+                                {"label": "Disabled option", "value": "3", "disabled": True},
+                            ],
+                        ),
                     ],
                     
                     style = {
