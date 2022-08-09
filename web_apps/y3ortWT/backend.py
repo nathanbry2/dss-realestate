3169dadc2617326771e96c844edcb01a271aef0a
admin <admin>
2022-08-09 10:45:55 +0200

    Saved web app v3 (REALESTATE.y3ortWT)

--- web_apps/y3ortWT/backend.py
+++ web_apps/y3ortWT/backend.py
@@ -219,7 +219,7 @@ app.layout = html.Div(
                                 {"label": "2020", "value": 2020},
                                 {"label": "2021", "value": 2021},
                             ],
-                            #value='2021',
+                            value='2021',
                             #placeholder="Year, e.g. 2021",
                             size='lg',
                             style = {
