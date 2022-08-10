abecc0d69840491c26684471fa74e4d202721201
admin <admin>
2022-08-10 11:15:35 +0200

    Saved web app v4 (REALESTATE.RbecpI4)

--- web_apps/RbecpI4/backend.py
+++ web_apps/RbecpI4/backend.py
@@ -577,7 +577,7 @@ def output_function(n_clicks,input1,input2,input3,input4,input5):
         margin={"r":30,"t":50,"l":30,"b":0},
         title_text="<i>Transactions Count</i> and <i>Average m² Price</i> evolution in zone <b>"+df_filtered.iloc[0]['NOM_IRIS_first']+"</b>",
         plot_bgcolor='rgba(0,0,0,0)',
-        title_font_size=18
+        title_font_size=19
         
     )
 
@@ -585,8 +585,8 @@ def output_function(n_clicks,input1,input2,input3,input4,input5):
     fig.update_xaxes(title_text="<b>Year</b>")
 
     # Set y-axes titles
-    fig.update_yaxes(title_text="<b>Average m² price</b>", secondary_y=True,ticksuffix='€')
-    fig.update_yaxes(title_text="<b>Transactions count</b>", secondary_y=False)
+    fig.update_yaxes(title_text="<b>Average m² price</b>", secondary_y=True,ticksuffix='€',title_font_size=10)
+    fig.update_yaxes(title_text="<b>Transactions count</b>", secondary_y=False,title_font_size=10)
     
     
     ### CREATE MAP CHART
